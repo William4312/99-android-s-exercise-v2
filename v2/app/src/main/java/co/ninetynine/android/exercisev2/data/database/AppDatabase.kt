aa27bf633498650114c749382a3b52d69c2db5ce
William4312 <gunawanandreas02@gmail.com>
2023-12-18 01:55:02 +0700

    baru modifikasi pertama

--- v2/app/src/main/java/co/ninetynine/android/exercisev2/data/database/AppDatabase.kt
+++ v2/app/src/main/java/co/ninetynine/android/exercisev2/data/database/AppDatabase.kt
@@ -3,5 +3,5 @@ package co.ninetynine.android.exercisev2.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ListingItemEntity::class], version = 1)
+@Database(entities = [ListingItemEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase()
